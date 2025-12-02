0726a58c471d9ce4ac196a67ada0d14b90a65696
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2025-12-02 22:21:07 +0530

    User Model changes

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -4,13 +4,15 @@ import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     hospital: { type: mongoose.Schema.Types.ObjectId, ref: 'Hospital', required: true, index: true },
     email: { type: String, required: true, trim: true, lowercase: true },
-    passwordHash: { type: String, required: true },
+    password: { type: String, required: true },
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     phone: { type: String },
     role: {
     type: String,
     enum: ['doctor','nurse','admin','receptionist','lab_technician','pharmacist'],
+    refreshToken: {type: String, required: false},
+    accessToken: {type: String, required: false},
     required: true,
     },
     isActive: { type: Boolean, default: true },
